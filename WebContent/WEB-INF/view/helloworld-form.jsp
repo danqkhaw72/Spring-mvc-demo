08fd0ce59eaa42ad6ee8da588f97caaa13bf5e8e
Dang Kham <danqkhaw72@gmail.com>
2020-07-20 14:51:31 +0700

    Binding request Params

--- WebContent/WEB-INF/view/helloworld-form.jsp
+++ WebContent/WEB-INF/view/helloworld-form.jsp
@@ -8,7 +8,7 @@
 </head>
 <body>
 
-	<form action="processFormVersionTwo" method="GET">
+	<form action="processFormVersionThree" method="GET">
 	
 		<input type="text" name="studentName"
 			placeholder="What's you name?" />
--- src/com/kamu/springdemo/mvc/HelloWorldController.java
+++ src/com/kamu/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -33,6 +34,23 @@ public class HelloWorldController {
 		
 		// create the message
 		String result = "Yo! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName
+			, Model model) {
+		
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey my friend form v3! " + theName;
 		
 		// add message to the model
 		model.addAttribute("message", result);
